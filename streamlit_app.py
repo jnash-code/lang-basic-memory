93c825a477560ea6a274cf2054408b4d5e8d1a10
jnash-code <111412746+jnash-code@users.noreply.github.com>
2024-10-13 20:20:20 +0000

    Use code from the docs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,47 @@
 import streamlit as st
+from langchain_community.chat_message_histories import (
+    StreamlitChatMessageHistory,
+)
 
-"Branch 2 Can be edited from the same codespace as branch 1"
+# My own code, used to test
+#st.secrets["OPENAI_API_KEY"]
 
-"Discard all changes! Check if it is possible to push after add this current line."
+# Optionally, specify your own session_state key for storing messages
+msgs = StreamlitChatMessageHistory(key="special_app_key")
+
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I help you?")
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an AI chatbot having a conversation with a human."),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ]
+)
+
+chain = prompt | ChatOpenAI(model="gpt-4o-mini")
+
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: msgs,  # Always return the instance created earlier
+    input_messages_key="question",
+    history_messages_key="history",
+)
+
+import streamlit as st
+
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+if prompt := st.chat_input():
+    st.chat_message("human").write(prompt)
+
+    # As usual, new messages are added to StreamlitChatMessageHistory when the Chain is called.
+    config = {"configurable": {"session_id": "any"}}
+    response = chain_with_history.invoke({"question": prompt}, config)
+    st.chat_message("ai").write(response.content)
